f03c5565d62671a1d56120efa9fafeefe6d17aec
beichen <826237710@qq.com>
2019-08-28 13:58:48 +0800

    feat(component): 使用 hooks 对组件进行改写

--- src/pages/display/index.js
+++ src/pages/display/index.js
@@ -4,13 +4,13 @@
  * @description: 用来展示 mobx 同步/异步操作数据的界面
  */
 
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import ProtoTypes from 'prop-types';
-import { observer } from 'mobx-react';
+import { useLocalStore, useObserver } from 'mobx-react';
 import { Button, Table } from 'antd';
 import DisplayModel from './model';
 
-const model = new DisplayModel();
+const store = new DisplayModel();
 
 // Table 的 columns 值
 const columns = [
@@ -32,44 +32,43 @@ const columns = [
     }
 ];
 
-@observer
-class Display extends Component {
+const Display = ({history}) => {
 
-    static propTypes = {
-        history: ProtoTypes.shape({
-            goBack: ProtoTypes.func.isRequired
-        }).isRequired
-    }
+    const model = useLocalStore(() => store);
 
-    componentDidMount() {
+    useEffect(() => {
         model.getListData();
-    }
+    }, []);
 
-    render() {
-        const tableTitle = () => <span>展示 Mobx 异步操作获取数据的 Table</span>;
-        const { count, changeCount, loading, listData } = model;
-        return (
-            <div>
-                <h2>Display 界面：展示 Mobx 同步、异步修改数据</h2>
-                <div style={{ marginTop: 20, marginBottom: 50 }}>
-                    <span>counter 计数器次数：<span style={{ background: 'pink' }}>{ count }</span></span>
-                    <br />
-                    <Button onClick={ changeCount.bind(model, 'increment') }>+</Button>
-                    <Button onClick={ changeCount.bind(model, 'decrement') } style={{ marginLeft: 30 }}>-</Button>
-                </div>
-                <Button style={{ marginBottom: 20 }} type="primary" onClick={ () => model.getListData() }>重新查询</Button>
-                <Button style={{ marginLeft: 30, marginBottom: 20 }} onClick={ () => this.props.history.goBack() }>返回</Button>
-                <Table
-                    title={ tableTitle }
-                    columns={ columns }
-                    dataSource={ listData.slice() }
-                    loading={ loading }
-                    bordered
-                    rowKey={ ({ address, number }) => address + number }
-                />
+    const tableTitle = () => <span>展示 Mobx 异步操作获取数据的 Table</span>;
+    const { changeCount, loading, getListData } = model;
+    return useObserver(() => (
+        <div>
+            <h2>Display 界面：展示 Mobx 同步、异步修改数据</h2>
+            <div style={{ marginTop: 20, marginBottom: 50 }}>
+                <span>counter 计数器次数：<span style={{ background: 'pink', padding: '4px 16px' }}>{ model.count }</span></span>
+                <br />
+                <Button onClick={ changeCount.bind(model, 'increment') }>+</Button>
+                <Button onClick={ changeCount.bind(model, 'decrement') } style={{ marginLeft: 30 }}>-</Button>
             </div>
-        );
-    }
-}
+            <Button style={{ marginBottom: 20 }} type="primary" onClick={ () => getListData() }>重新查询</Button>
+            <Button style={{ marginLeft: 30, marginBottom: 20 }} onClick={ () => history.goBack() }>返回</Button>
+            <Table
+                title={ tableTitle }
+                columns={ columns }
+                dataSource={ model.listData.slice() }
+                loading={ loading }
+                bordered
+                rowKey={ ({ address, number }) => address + number }
+            />
+        </div>
+    ));
+};
+
+Display.propTypes = {
+    history: ProtoTypes.shape({
+        goBack: ProtoTypes.func.isRequired
+    }).isRequired
+};
 
 export default Display;
--- src/pages/settings/index.js
+++ src/pages/settings/index.js
@@ -4,74 +4,61 @@
  * @description: 用来展示和修改全局 Model 数据使用方式的界面
  */
 
-import React, { Component } from 'react';
-import { inject, observer } from 'mobx-react';
+import React, { useState } from 'react';
+import { inject } from 'mobx-react';
 import { Button } from 'antd';
 import PropTypes from 'prop-types';
 import { Card } from 'antd';
 
-@inject('globalModel')
-@observer
-class Seetings extends Component {
 
-    static propTypes = {
-        globalModel: PropTypes.shape({
-            username: PropTypes.string.isRequired,
-            changeUserName: PropTypes.func.isRequired
-        }).isRequired,
-        history: PropTypes.shape({
-            goBack: PropTypes.func.isRequired
-        }).isRequired
-    }
+const Settings = ({globalModel, history}) => {
 
-    constructor(props) {
-        super(props);
-        // 定义 val，让 input 成为受控组件
-        this.state = {
-            val: ''
-        };
-    }
+    // 定义 val，让 input 成为受控组件
+    const [val, setVal] = useState('');
 
     /**
      * input 值发生变化时触发该方法
      */
-    handleValueChange = (e) => {
-        this.setState({
-            val: e.target.value
-        });
-    }
+    const handleValueChange = (e) => {
+        setVal(e.target.value);
+    };
 
     /**
      * 按钮点击时触发该方法
      */
-    handleClick = () => {
-        const { val } = this.state;
-        const { changeUserName } =  this.props.globalModel;
+    const handleClick = () => {
+        const { changeUserName } = globalModel;
         if (val) {
             changeUserName(val);
-            this.setState({
-                val: ''
-            });
+            setVal('');
         }
-    }
+    };
 
-    render () {
-        const { username } = this.props.globalModel;
-        const { val } = this.state;
-        const title = "Settings 界面：展示以及修改全局 Model 中的属性";
-        const actions = [ <Button key="0" onClick={ () => this.props.history.goBack() }>返回</Button> ];
-        return (
-            <Card title={ title } actions={ actions } style={{ width: 600 }}>
-                <div style={{ marginBottom: 30 }}>
-                    <input placeholder="输入新的 username 值" value={ val } onChange={ this.handleValueChange } />
-                    <Button style={{ marginLeft: 20 }} type="primary" onClick={ this.handleClick }>修改 username </Button>
-                </div>
-                <span>可以获取到 GlobalModel 中的 username: 
-                    <span style={{ marginLeft: 20, background: 'pink' }}>{ username }</span>
-                </span>
-            </Card>
-        );
-    }
-}
+    const { username } = globalModel;
+    const title = "Settings 界面：展示以及修改全局 Model 中的属性";
+    const actions = [ <Button key="0" onClick={ () => history.goBack() }>返回</Button> ];
 
-export default Seetings;
+    return (
+        <Card title={ title } actions={ actions } style={{ width: 600 }}>
+            <div style={{ marginBottom: 30 }}>
+                <input placeholder="输入新的 username 值" value={ val } onChange={ handleValueChange } />
+                <Button style={{ marginLeft: 20 }} type="primary" onClick={ handleClick }>修改 username </Button>
+            </div>
+            <span>可以获取到 GlobalModel 中的 username: 
+                <span style={{ marginLeft: 20, background: 'pink', padding: '4px 16px' }}>{ username }</span>
+            </span>
+        </Card>
+    );
+};
+
+Settings.propTypes = {
+    globalModel: PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        changeUserName: PropTypes.func.isRequired
+    }).isRequired,
+    history: PropTypes.shape({
+        goBack: PropTypes.func.isRequired
+    }).isRequired
+};
+
+export default inject('globalModel')(Settings);
